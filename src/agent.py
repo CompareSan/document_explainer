6f24c9bdf7f1354e96fa0d4fd5f56b4020fcee67
Filippo Buoncompagni <filippo.buoncompagni@multiversecomputing.com>
2024-02-22 15:14:55 +0100

    Add the web app

---
+++ src/agent.py
@@ -0,0 +1,59 @@
+import os
+
+from dotenv import (
+    find_dotenv,
+    load_dotenv,
+)
+from langchain.agents import (
+    Tool,
+    initialize_agent,
+)
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+load_dotenv(find_dotenv())
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+SERPAPI_API_KEY = os.environ.get("SERPAPI_API_KEY")
+
+
+def build_agent(docs):
+
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    llm = ChatOpenAI(
+        temperature=0.0, model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY
+    )
+
+    retaining_memory = ConversationBufferWindowMemory(
+        memory_key="chat_history", k=5, return_messages=True
+    )
+
+    db = Chroma.from_documents(docs, embeddings)
+
+    question_answering = ConversationalRetrievalChain.from_llm(
+        llm, retriever=db.as_retriever(), memory=retaining_memory
+    )
+
+    tools = [
+        Tool(
+            name="Knowledge Base",
+            func=question_answering.run,
+            description=("use this tool when answering questions related to pdf file"),
+        )
+    ]
+
+    agent = initialize_agent(
+        agent="chat-conversational-react-description",
+        tools=tools,
+        llm=llm,
+        verbose=True,
+        max_iterations=3,
+        early_stopping_method="generate",
+        memory=retaining_memory,
+    )
+
+    return agent
--- src/main.py
+++ src/main.py
@@ -1,70 +1,18 @@
-import os
-
-from dotenv import (
-    find_dotenv,
-    load_dotenv,
-)
-from langchain.agents import (
-    Tool,
-    initialize_agent,
-)
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import PyPDFLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-
-load_dotenv(find_dotenv())
+from agent import build_agent
+from ingest import load_document
 
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-SERPAPI_API_KEY = os.environ.get("SERPAPI_API_KEY")
+def main(file_path):
 
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-llm = ChatOpenAI(
-    temperature=0.0, model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY
-)
+    document = load_document(file_path)
+    agent = build_agent(document)
 
-retaining_memory = ConversationBufferWindowMemory(
-    memory_key="chat_history", k=5, return_messages=True
-)
-
-loader = PyPDFLoader("../FILIPPO_EPSOP.pdf")
-pages = loader.load()
-
-document_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=70)
+    while True:
+        question = input("Enter your query: ")
+        if question == "exit":
+            break
+        print(agent(question))
 
 
-docs = document_splitter.split_documents(pages)
-
-db = Chroma.from_documents(pages, embeddings)
-
-question_answering = ConversationalRetrievalChain.from_llm(
-    llm, retriever=db.as_retriever(), memory=retaining_memory
-)
-
-tools = [
-    Tool(
-        name="Knowledge Base",
-        func=question_answering.run,
-        description=("use this tool when answering questions related to pdf file"),
-    )
-]
-
-agent = initialize_agent(
-    agent="chat-conversational-react-description",
-    tools=tools,
-    llm=llm,
-    verbose=True,
-    max_iterations=3,
-    early_stopping_method="generate",
-    memory=retaining_memory,
-)
-
-while True:
-    question = input("Enter your query: ")
-    if question == "exit":
-        break
-    print(agent(question))
+if __name__ == "__main__":
+    main("/Users/filippobuoncompagni/document_explainer/FILIPPO_EPSOP.pdf")
